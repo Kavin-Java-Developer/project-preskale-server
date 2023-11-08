875f4b7194eec7115c79e1bc97682db4f5745240
Kavin Kumar S <125345911+Kavin-Java-Developer@users.noreply.github.com>
2023-11-08 10:01:49 +0530

    Update data.js

--- database/data.js
+++ database/data.js
@@ -1,49 +1,248 @@
-export default [
+export default[
     {
         id: 1,
-        question : "Javascript is an _______ language",
+        question : "Javascript is an ________ language",
         options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
+            'object-oriented',
+            'object-based',
+            'procedural',
         ]
     },
     {
-        id: 2,
-        question : "Following methods can be used to display data in some form using Javascript",
+        id:2,
+        question : "What does DOM stand for",
         options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
+            'Document Object Model',
+            ' Data Object Model',
+            'Document Object Manipulation',
+         
         ]
     },
     {
-        id: 3,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        id:3,
+        question : " Which keyword is used to declare variables in JavaScript",
         options : [
-            'Boolean',
-            'Undefined',
-            'Object',
+            'var',
+            'let',
+            'const',
+         
         ]
     },
     {
-        id: 4,
-        question : "What does the toString() method return?",
+        id:4,
+        question : "What is the purpose of the parseInt() function in JavaScript",
         options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
+            'To convert a string to an integer',
+            'To round a number to the nearest integer',
+            'To convert a number to a string',
+         
         ]
     },
     {
-        id: 5,
-        question : "Which function is used to serialize an object into a JSON string?",
+        id:5,
+        question : "Which operator is used for strict equality in JavaScript",
         options : [
-            'stringify()',
-            'parse()',
-            'convert()',
+            '==',
+            '===',
+            '=',
         ]
-    }
-];
+    },
+    {
+        id:6,
+        question : "Which loop is used to iterate over the properties of an object in JavaScript",
+        options : [
+            'for loop',
+            'while loop',
+            'for...in loop',
+         
+         
+        ]
+    },
+    {
+        id:7,
+        question : "What is an example of a JavaScript data type",
+        options : [
+              'String',
+                'Bold',
+            'Highlight',
 
-export const answers = [0, 1, 2, 1, 0];
+        ]
+    },
+    {
+        id:8,
+        question : "Which method is used to add an element to the end of an array in JavaScript",
+        options : [
+            'push',
+            'pop',
+            'shift',
+         
+         
+        ]
+    },
+    {
+        id:9,
+        question : "What is an example of a JavaScript framework/library for building user interfaces",
+        options : [
+            'React',
+            'JavaFX',
+            'Django',
+         
+        ]
+    },
+    {
+        id:10,
+        question : "Which operator is used for strict equality in JavaScript What does AJAX stand for in the context of web development",
+        options : [
+             'Asynchronous JavaScript and XML',
+            'Advanced JavaScript and XHTML',
+             'Automated JSON and XML',
+        ]
+    },
+    {
+        id:11,
+        question : "Which JavaScript event occurs when an element is clicked",
+        options : [
+    'onhover',
+    'onclick',
+    'onmouseover',
+
+        ]
+    },
+    {
+        id:12,
+        question : "What does JSON stand for",
+        options : [
+             'JavaScript Object Notation',
+            'JavaScript Object Network',
+            'JavaScript Object Namespace',
+        
+        ]
+    },
+    {
+        id:13,
+        question : "Which keyword is used to define a function in JavaScript",
+        options : [
+             'func',
+            'def',
+             'function',
+        
+        ]
+    },
+    {
+        id:14,
+        question : "Which operator is used for strict equality in JavaScript",
+        options : [
+            'pop()',
+            'push()',
+            'shift()',
+        
+        ]
+    },
+    {
+        id:15,
+        question : "Which opeWhat is a closure in JavaScript",
+        options : [
+            'A function that has access to variables from its outer scope',
+             'A variable with global scope',
+            'A built-in JavaScript class',
+        
+        ]
+    },
+    {
+        id:16,
+        question : " What does the typeof operator return for an array",
+        options : [
+            'array',
+            'object',
+            'list',
+        ]
+    },
+    {
+        id:17,
+        question : "Which statement is used to exit a loop in JavaScript",
+        options : [
+            'continue',
+            'break',
+            'return',
+        
+        ]
+    },
+    {
+        id:18,
+        question : " What is the purpose of the querySelector() method in JavaScript",
+        options : [
+            'To select multiple elements in the DOM',
+            'To select the first element that matches a CSS selector',
+            'To query the user for input',
+        ]
+    },
+    {
+        id:19,
+        question : "Which event is triggered when an input field loses focus in JavaScript",
+        options : [
+            'onblur',
+            'onfocus',
+            'onclick',
+        
+        ]
+    },
+    {
+        id:20,
+        question : "What is the purpose of the setTimeout() function in JavaScript",
+        options : [
+            'To set the current date and time',
+            'To delay the execution of a function',
+            'To set a timer for an animation',
+        
+        ]
+    },
+    {
+        id:21,
+        question : " What is the purpose of the NaN value in JavaScript",
+        options : [
+            'It stands for "Not a Name',
+            'It represents "Not a Numbe',
+            'It is used for naming functions',
+        
+        ]
+    },
+    {
+        id:22,
+        question : "Which method is used to join elements of an array into a string in JavaScript",
+        options : [
+            'split()',
+            'concat()',
+            'join()',
+        
+        ]
+    },
+    {
+        id:23,
+        question : "What is the global object in a web browser's JavaScript environment",
+        options : [
+            'document',
+            'window',
+            'navigato',
+        ]
+    },
+    {
+        id:24,
+        question : "What does the localStorage object in JavaScript allow you to do",
+        options : [
+            'Store data in a local file',
+            'Store data on a remote server',
+            'Store data in the users browser',
+        
+        ]
+    },
+    {
+        id:25,
+        question : "What does the fetch() function in JavaScript allow you to do",
+        options : [
+            'Make network requests and retrieve data',
+            'Create a new DOM element',
+            'Calculate mathematical expressions',
+        ]
+    },
+    
+]
